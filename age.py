58fe43bd897db25a286190ca15515297d73cff9b
soluke22 <soluke22@gmail.com>
2018-02-16 12:40:46 -0500

    Create age.py

---
+++ age.py
@@ -0,0 +1,7 @@
+name= input("Give me your name: ")
+print ("Your name is " + name)
+age = int(input("How old are you:"))
+yearthatyouwillbe100 = int(2018 + 100 - age)
+print (name + ", You will be 100 in the year " + str(yearthatyouwillbe100))
+
+
