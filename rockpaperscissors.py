91c8fccaa7e004fff727a5c03736f7bbc172bb17
soluke22 <soluke22@gmail.com>
2018-02-21 18:40:27 -0500

    rockpaperscissors1.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,5 +1,5 @@
 
-#Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+#make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
 while True:
   print("Welcome to Two-Player Rock-Paper-Scissors!")
