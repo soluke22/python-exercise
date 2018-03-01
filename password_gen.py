fbeb4884d1a47c7744ecd8dca9bd984645126d14
soluke22 <soluke22@gmail.com>
2018-03-01 13:31:10 -0500

    Create password_gen.py

---
+++ password_gen.py
@@ -0,0 +1,7 @@
+#Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random, generating a new password every time the user asks for a new password. Include your run-time code in a main method.
+import random
+
+pass_chars= "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+pass_len = int(input("How long do you want your password to be?:"))
+pass = "".join(random.sample(pass_chars,pass_len))
+print(pass)
